b0f5ae2e58815c4b6530712ef67d8341eb2e3ce1
narekas <apresyan026@gmail.com>
2022-10-07 20:54:06 +0400

    New commit

--- scrap.py
+++ scrap.py
@@ -1,44 +1,27 @@
-import os
-import time
-
 import requests
 from bs4 import BeautifulSoup
+from time import sleep
+
+list_card_url = []
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
+}
+
+for page in range(1, 2):
+    sleep(1)
+    url = f'http://www.noyantapan.am/catalog/books/?PAGE_1=1&PAGEN_1={page}'
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, 'lxml')
+    data = soup.find_all('div', class_='item product sku')[:30]
+    for item in data:
+        card_url = 'http://www.noyantapan.am' + item.find('a', class_='name').get('href')
+        list_card_url.append(card_url)
 
 
-def get_all_pages():
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
-    }
+for card_url in list_card_url:
+    response = requests.get(card_url, headers=headers)
+    soup = BeautifulSoup(response.text, 'lxml')
+    data = soup.find('div', class_='mainContainer')
+    code = ''.join(data.find('div', class_='propertyTable').text.split())
+    print(code)
 
-    # request = requests.get(url='http://www.noyantapan.am/catalog/books/', headers=headers)
-
-    # if not os.path.exists('data1'):
-    #    os.mkdir('data1')
-
-    # with open('data1/page_1.html', 'w') as file:
-    #    file.write(request.text)
-
-    with open('data1/page_1.html') as file:
-        src = file.read()
-
-    soup = BeautifulSoup(src, 'lxml')
-    pages_count = int(soup.find('div', class_='bx-pagination-container row').find_all('a')[-2].text)
-
-    for page in range(1, pages_count + 1):
-        url = f'http://www.noyantapan.am/catalog/books/?PAGEN_1={page}'
-
-        r = requests.get(url=url, headers=headers)
-
-        with open(f'data1/page_{page}.html', 'w') as file:
-            file.write(r.text)
-
-        time.sleep(2)
-    return pages_count + 1
-
-
-def main():
-    get_all_pages()
-
-
-if __name__ == '__main__':
-    main()
