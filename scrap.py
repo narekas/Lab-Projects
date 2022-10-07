4090f86cbe8cfc41a7759a6bd1877ab79191d0cd
narekas <apresyan026@gmail.com>
2022-10-07 12:25:02 +0400

    New commit

--- scrap.py
+++ scrap.py
@@ -1,15 +1,44 @@
+import os
+import time
+
 import requests
 from bs4 import BeautifulSoup
 
-for count in range(1, 287):
 
-    url = f'http://www.noyantapan.am/catalog/books/?PAGEN_1={count}'
+def get_all_pages():
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
+    }
 
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
-    data = soup.find_all('div', class_='item product sku')
-    for items in data:
-        name = items.find('span', class_='middle').text
-        price = items.find('a', class_='price').text
-        article = 'http://www.noyantapan.am' + items.find('a', class_='picture').get('href')
-        print(name, price, article, sep='\n')
+    # request = requests.get(url='http://www.noyantapan.am/catalog/books/', headers=headers)
+
+    # if not os.path.exists('data1'):
+    #    os.mkdir('data1')
+
+    # with open('data1/page_1.html', 'w') as file:
+    #    file.write(request.text)
+
+    with open('data1/page_1.html') as file:
+        src = file.read()
+
+    soup = BeautifulSoup(src, 'lxml')
+    pages_count = int(soup.find('div', class_='bx-pagination-container row').find_all('a')[-2].text)
+
+    for page in range(1, pages_count + 1):
+        url = f'http://www.noyantapan.am/catalog/books/?PAGEN_1={page}'
+
+        r = requests.get(url=url, headers=headers)
+
+        with open(f'data1/page_{page}.html', 'w') as file:
+            file.write(r.text)
+
+        time.sleep(2)
+    return pages_count + 1
+
+
+def main():
+    get_all_pages()
+
+
+if __name__ == '__main__':
+    main()
