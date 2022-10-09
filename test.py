0315f73bd0e0645598bb559d9406f54da430570b
Nar Apresyan <108577622+narekas@users.noreply.github.com>
2022-10-10 01:07:14 +0400

    Update test.py

--- test.py
+++ test.py
@@ -58,3 +58,233 @@ for book in books_list:
     with open('noyantapan.csv', 'a') as file:
         writer = csv.writer(file)
         writer.writerow([book.summary.get(decisive) for decisive in column_list])
+
+
+
+# test 2
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+headers = {
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
+}
+
+
+def get_data():
+    with open('zangak.csv', 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(
+            (
+                'title',
+                'author',
+                'publisher',
+                'price',
+                'language',
+                'pages',
+                'genre',
+                'category',
+                'target',
+                'EAN',
+                'code',
+                'year',
+                'url'
+
+            )
+        )
+    for page in range(1, 920):
+        url = f'https://zangakbookstore.am/grqer?page={page}'
+        response  = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.text, 'lxml')
+        data = soup.find('div', class_='row no-gutters product-items-list items-list-container')
+        cards = data.find_all('div', class_='col-6 col-md-4 col-lg-6 col-xl-4 col-xxl-3 mb-5 list-item')
+        for c_url in cards:
+            cards_url = c_url.find('a', class_='d-inline-block position-relative').get('href')
+            yield cards_url
+
+
+for card_url in get_data():
+    response = requests.get(card_url, headers=headers)
+    soup = BeautifulSoup(response.text, 'lxml')
+    try:
+        title = soup.find('h1', class_='product-name mb-4 d-none d-md-block').text
+    except:
+        title = ''
+    print(title)
+    try:
+        price = soup.find('div', class_='product-price-view mb-2').text
+    except:
+        price = ''
+    try:
+        author = soup.find('a', class_='color-main text-decoration-none author-btn').text
+    except:
+        author = ''
+    try:
+        found = soup.find('div', id='tab_details', class_='tab-pane fade show active')
+        if found == None:
+            found = soup.find('div', id='tab_details', class_='tab-pane fade ')
+        table = soup.find_all('div', class_='form-row')
+    except:
+        table = ''
+    try:
+        summary = [couple.find_all('div')[1].text for couple in table]
+    except:
+        summary = ''
+    try:
+        publisher = summary[0]
+    except:
+        publisher = ''
+    try:
+        language = summary[4]
+    except:
+        language = ''
+    try:
+        pages = summary[6]
+    except:
+        pages = ''
+    try:
+        cover = summary[5]
+    except:
+        cover = ''
+    try:
+        genre = soup.find_all('li', class_='breadcrumb-item')[1].text
+    except:
+        genre = ''
+    try:
+        category = soup.find_all('li', class_='breadcrumb-item')[2].text
+    except:
+        category = ''
+    try:
+        target = summary[9]
+    except:
+        target = ''
+    try:
+        EAN = summary[1]
+    except:
+        EAN = ''
+    try:
+        code = summary[2]
+    except:
+        code = ''
+    try:
+        year = summary[3]
+    except:
+        year = ''
+    try:
+        book_url = card_url
+    except:
+        book_url = ''
+    with open('zangak.csv', 'a') as file:
+        writer = csv.writer(file)
+        writer.writerow(
+            (
+                title,
+                author,
+                publisher,
+                price,
+                language,
+                pages,
+                genre,
+                category,
+                target,
+                EAN,
+                code,
+                year,
+                book_url
+
+            )
+        )
+
+# test 3
+import requests
+from bs4 import BeautifulSoup
+import csv
+import sys
+
+headers = {
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
+}
+
+
+def get_data():
+    with open('zangak.csv', 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(
+            (
+                'title',
+                'author',
+                'publisher',
+                'price',
+                'language',
+                'pages',
+                'genre',
+                'category',
+                'target',
+                'EAN',
+                'code',
+                'year',
+                'url'
+
+            )
+        )
+    for page in range(1, 20):
+        url = f'https://zangakbookstore.am/grqer?page={page}'
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.text, 'lxml')
+        data = soup.find('div', class_='row no-gutters product-items-list items-list-container')
+        cards = data.find_all('div', class_='col-6 col-md-4 col-lg-6 col-xl-4 col-xxl-3 mb-5 list-item')
+        for c_url in cards:
+            cards_url = c_url.find('a', class_='d-inline-block position-relative').get('href')
+            yield cards_url
+
+
+for card_url in get_data():
+    response = requests.get(card_url, headers=headers)
+    soup = BeautifulSoup(response.text, 'lxml')
+    try:
+        title = soup.find('h1', class_='product-name mb-4 d-none d-md-block').text
+        print(title)
+
+        price = soup.find('div', class_='product-price-view mb-2').text
+        try:
+            author = soup.find('a', class_='color-main text-decoration-none author-btn').text
+        except:
+            author = str()
+
+        found = soup.find('div', id='tab_details', class_='tab-pane fade show active')
+        if found == None:
+            found = soup.find('div', id='tab_details', class_='tab-pane fade ')
+        table = soup.find_all('div', class_='form-row')
+
+        summary = [couple.find_all('div')[1].text for couple in table]
+
+        publisher, EAN, code, year, language, cover, pages = summary[:7]
+
+        genre = soup.find_all('li', class_='breadcrumb-item')[1].text
+        category = soup.find_all('li', class_='breadcrumb-item')[2].text
+        target = summary[9]
+        book_url = card_url
+    except:
+        print(card_url, file=sys.stderr)
+        continue
+
+    with open('zangak.csv', 'a') as file:
+        writer = csv.writer(file)
+        writer.writerow(
+            (
+                title,
+                author,
+                publisher,
+                price,
+                language,
+                pages,
+                genre,
+                category,
+                target,
+                EAN,
+                code,
+                year,
+                book_url
+
+            )
+        )
